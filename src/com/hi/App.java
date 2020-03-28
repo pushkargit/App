da85d854e572feda8f4b830d01761cfa500c8588
pushkargit <55329546+pushkargit@users.noreply.github.com>
2020-03-29 00:36:59 +0530

    Add files via upload

---
+++ src/com/hi/App.java
@@ -0,0 +1,23 @@
+package com.hi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+
+	public static void main(String args[]) {
+		System.out.println("hello there ");
+		List<Integer> list;
+		list = new ArrayList<>();
+		
+		
+	}
+	
+	public static void hello() {
+		
+		System.out.println("hello there what is going on in the world");
+		System.out.println("as the name says , we have to keep it work ");
+		System.out.println("to make the world a new symbol for the people");
+		    
+	}
+}
